440089c0c95ccc54ada603e0e42e322e7c1d0889
9teMare <wang.zihao@u.nus.edu>
2023-10-05 01:18:37 +0800

    Update create_pcsp_model.py

--- create_pcsp_model.py
+++ create_pcsp_model.py
@@ -9,7 +9,11 @@ TEMPLATE_FILENAME = "template.pcsp"
 PLAYER_RATINGS_GENERATOR = {
     "shortPass": (lambda player: player["attacking_short_passing"]),
     "longPass": (lambda player: player["skill_long_passing"]),
-    "probLoseBall": (lambda player_for, player_against: generate_player_probLoseBall(player_for, player_against)),
+    "probLoseBall": (
+        lambda player_for, player_against: generate_player_probLoseBall(
+            player_for, player_against
+        )
+    ),
     "longShot": (lambda player: player["power_long_shots"]),
     "finish": (lambda player: player["attacking_finishing"]),
     "volley": (lambda player: player["attacking_volleys"]),
@@ -66,23 +70,24 @@ SRC_DIR = "./Datasets"
 MATCHES_DIR = f"{SRC_DIR}/matches"
 RATINGS_DIR = f"{SRC_DIR}/ratings"
 
-KEEPER_POSITIONS = ['GK']
-DEFENDER_POSITIONS = ['RD', 'CRD', 'CLD', 'LD']
-MIDFIELDER_POSITIONS = ['RM', 'CM', 'LM']
-FORWARD_POSITIONS = ['RF', 'CF', 'LF']
+KEEPER_POSITIONS = ["GK"]
+DEFENDER_POSITIONS = ["RD", "CRD", "CLD", "LD"]
+MIDFIELDER_POSITIONS = ["RM", "CM", "LM"]
+FORWARD_POSITIONS = ["RF", "CF", "LF"]
 
 DEFENDING_POSITION_MAPPER = {
-    'RD': 'LM',
-    'CRD': 'LF',
-    'CLD': 'CF',
-    'LD': 'RF',
-    'RM': 'CM',
-    'CM': 'RM',
-    'LM': 'RD',
-    'RF': 'LD',
-    'CF': 'CLD',
-    'LF': 'CRD'
+    "RD": "LM",
+    "CRD": "LF",
+    "CLD": "CF",
+    "LD": "RF",
+    "RM": "CM",
+    "CM": "RM",
+    "LM": "RD",
+    "RF": "LD",
+    "CF": "CLD",
+    "LF": "CRD",
 }
+
 
 def create_pcsp_model(filename: str, data: Dict[str, int]) -> None:
     # Validate data to check if it has the relevant data
@@ -104,14 +109,14 @@ def create_pcsp_model(filename: str, data: Dict[str, int]) -> None:
     except KeyError as e:
         print(f"Key error: {e} not found in data.")
         return
-    
+
     # Define the sub-folder path
     sub_folder_path = "Generated_Models"
-    
+
     # Create sub-folder if it does not exist
     if not os.path.exists(sub_folder_path):
         os.makedirs(sub_folder_path)
-    
+
     # Combine the sub-folder path with the desired filename
     file_path = os.path.join(sub_folder_path, filename)
 
@@ -176,6 +181,7 @@ def get_match_sofifa_ids(matches_df: pd.DataFrame, matchId):
         },
     }
 
+
 def get_player_rating_by_sofifa_id(rating_df: pd.DataFrame, sofifa_id):
     for _, row in rating_df.iterrows():
         if "sofifa_id" in row and int(float(row["sofifa_id"])) == sofifa_id:
@@ -204,11 +210,12 @@ def get_matches_cli(matches_df: pd.DataFrame):
         matches.append(name)
     return matches
 
+
 def get_match_name(matches_df: pd.DataFrame, match_id: int):
     home_team_name = matches_df.iloc[match_id]["home_team"]
     away_team_name = matches_df.iloc[match_id]["away_team"]
 
-    return f'{home_team_name}_{away_team_name}'
+    return f"{home_team_name}_{away_team_name}"
 
 
 def print_player(rating_df: pd.DataFrame, players_in_match):
@@ -246,12 +253,19 @@ def print_player(rating_df: pd.DataFrame, players_in_match):
     for player in players_in_match["away_team_sofifa_ids"]["forwards"]:
         get_player_rating_by_sofifa_id(rating_df, player)
 
+
 def generate_data_dict_for_team(for_stats, against_stats):
     data = {}
     # keeper stats
-    data['ak_shortPass'] = PLAYER_RATINGS_GENERATOR["shortPass"](for_stats[KEEPER_POSITIONS[0]])
-    data['ak_longPass'] = PLAYER_RATINGS_GENERATOR["longPass"](for_stats[KEEPER_POSITIONS[0]])
-    data['dk_gkRating'] = PLAYER_RATINGS_GENERATOR["dk_gkRating"](against_stats[KEEPER_POSITIONS[0]])
+    data["ak_shortPass"] = PLAYER_RATINGS_GENERATOR["shortPass"](
+        for_stats[KEEPER_POSITIONS[0]]
+    )
+    data["ak_longPass"] = PLAYER_RATINGS_GENERATOR["longPass"](
+        for_stats[KEEPER_POSITIONS[0]]
+    )
+    data["dk_gkRating"] = PLAYER_RATINGS_GENERATOR["dk_gkRating"](
+        against_stats[KEEPER_POSITIONS[0]]
+    )
 
     # defender stats
     for position in DEFENDER_POSITIONS:
@@ -260,11 +274,13 @@ def generate_data_dict_for_team(for_stats, against_stats):
 
         short_pass_rating = PLAYER_RATINGS_GENERATOR["shortPass"](for_player)
         long_pass_rating = PLAYER_RATINGS_GENERATOR["longPass"](for_player)
-        prob_lose_ball = PLAYER_RATINGS_GENERATOR["probLoseBall"](for_player, against_player)
-        data[f'a{position.lower()}_shortPass'] = short_pass_rating
-        data[f'a{position.lower()}_longPass'] = long_pass_rating
-        data[f'a{position.lower()}_probLoseBall'] = prob_lose_ball
-    
+        prob_lose_ball = PLAYER_RATINGS_GENERATOR["probLoseBall"](
+            for_player, against_player
+        )
+        data[f"a{position.lower()}_shortPass"] = short_pass_rating
+        data[f"a{position.lower()}_longPass"] = long_pass_rating
+        data[f"a{position.lower()}_probLoseBall"] = prob_lose_ball
+
     # midfielder stats
     for position in MIDFIELDER_POSITIONS:
         for_player = for_stats[position]
@@ -273,12 +289,14 @@ def generate_data_dict_for_team(for_stats, against_stats):
         short_pass_rating = PLAYER_RATINGS_GENERATOR["shortPass"](for_player)
         long_pass_rating = PLAYER_RATINGS_GENERATOR["longPass"](for_player)
         long_shot_rating = PLAYER_RATINGS_GENERATOR["longShot"](for_player)
-        prob_lose_ball = PLAYER_RATINGS_GENERATOR["probLoseBall"](for_player, against_player)
-        data[f'a{position.lower()}_shortPass'] = short_pass_rating
-        data[f'a{position.lower()}_longPass'] = long_pass_rating
-        data[f'a{position.lower()}_longShot'] = long_shot_rating
-        data[f'a{position.lower()}_probLoseBall'] = prob_lose_ball
-    
+        prob_lose_ball = PLAYER_RATINGS_GENERATOR["probLoseBall"](
+            for_player, against_player
+        )
+        data[f"a{position.lower()}_shortPass"] = short_pass_rating
+        data[f"a{position.lower()}_longPass"] = long_pass_rating
+        data[f"a{position.lower()}_longShot"] = long_shot_rating
+        data[f"a{position.lower()}_probLoseBall"] = prob_lose_ball
+
     for position in FORWARD_POSITIONS:
         for_player = for_stats[position]
         against_player = against_stats[DEFENDING_POSITION_MAPPER[position]]
@@ -287,52 +305,83 @@ def generate_data_dict_for_team(for_stats, against_stats):
         volley_rating = PLAYER_RATINGS_GENERATOR["volley"](for_player)
         long_shot_rating = PLAYER_RATINGS_GENERATOR["longShot"](for_player)
         header_rating = PLAYER_RATINGS_GENERATOR["header"](for_player)
-        prob_lose_ball = PLAYER_RATINGS_GENERATOR["probLoseBall"](for_player, against_player)
-        data[f'a{position.lower()}_finish'] = finish_rating
-        data[f'a{position.lower()}_longShot'] = long_shot_rating
-        data[f'a{position.lower()}_volley'] = volley_rating
-        data[f'a{position.lower()}_header'] = header_rating
-        data[f'a{position.lower()}_probLoseBall'] = prob_lose_ball
-    
+        prob_lose_ball = PLAYER_RATINGS_GENERATOR["probLoseBall"](
+            for_player, against_player
+        )
+        data[f"a{position.lower()}_finish"] = finish_rating
+        data[f"a{position.lower()}_longShot"] = long_shot_rating
+        data[f"a{position.lower()}_volley"] = volley_rating
+        data[f"a{position.lower()}_header"] = header_rating
+        data[f"a{position.lower()}_probLoseBall"] = prob_lose_ball
+
     return data
 
+
 def generate_data_dict(rating_df: pd.DataFrame, players_in_match: Dict[str, dict]):
+    print("\n--------Generating player data dictionary--------")
     home_stats = {}
     away_stats = {}
 
     # Generate keeper stats
-    for i, player_id in enumerate(players_in_match["home_team_sofifa_ids"]["goalkeeper"]):
-        home_stats[KEEPER_POSITIONS[i]] = get_player_rating_by_sofifa_id(rating_df, player_id)
-    
-    for i, player_id in enumerate(players_in_match["away_team_sofifa_ids"]["goalkeeper"]):
-        away_stats[KEEPER_POSITIONS[i]] = get_player_rating_by_sofifa_id(rating_df, player_id)
-    
+    for i, player_id in enumerate(
+        players_in_match["home_team_sofifa_ids"]["goalkeeper"]
+    ):
+        home_stats[KEEPER_POSITIONS[i]] = get_player_rating_by_sofifa_id(
+            rating_df, player_id
+        )
+
+    for i, player_id in enumerate(
+        players_in_match["away_team_sofifa_ids"]["goalkeeper"]
+    ):
+        away_stats[KEEPER_POSITIONS[i]] = get_player_rating_by_sofifa_id(
+            rating_df, player_id
+        )
+
     # Generate defender stats
-    for i, player_id in enumerate(players_in_match["home_team_sofifa_ids"]["defenders"]):
-        home_stats[DEFENDER_POSITIONS[i]] = get_player_rating_by_sofifa_id(rating_df, player_id)
+    for i, player_id in enumerate(
+        players_in_match["home_team_sofifa_ids"]["defenders"]
+    ):
+        home_stats[DEFENDER_POSITIONS[i]] = get_player_rating_by_sofifa_id(
+            rating_df, player_id
+        )
 
-    for i, player_id in enumerate(players_in_match["away_team_sofifa_ids"]["defenders"]):
-        away_stats[DEFENDER_POSITIONS[i]] = get_player_rating_by_sofifa_id(rating_df, player_id)
-    
+    for i, player_id in enumerate(
+        players_in_match["away_team_sofifa_ids"]["defenders"]
+    ):
+        away_stats[DEFENDER_POSITIONS[i]] = get_player_rating_by_sofifa_id(
+            rating_df, player_id
+        )
+
     # Generate midfielder stats
-    for i, player_id in enumerate(players_in_match["home_team_sofifa_ids"]["midfielders"]):
-        home_stats[MIDFIELDER_POSITIONS[i]] = get_player_rating_by_sofifa_id(rating_df, player_id)
+    for i, player_id in enumerate(
+        players_in_match["home_team_sofifa_ids"]["midfielders"]
+    ):
+        home_stats[MIDFIELDER_POSITIONS[i]] = get_player_rating_by_sofifa_id(
+            rating_df, player_id
+        )
 
-    for i, player_id in enumerate(players_in_match["away_team_sofifa_ids"]["midfielders"]):
-        away_stats[MIDFIELDER_POSITIONS[i]] = get_player_rating_by_sofifa_id(rating_df, player_id)
-    
+    for i, player_id in enumerate(
+        players_in_match["away_team_sofifa_ids"]["midfielders"]
+    ):
+        away_stats[MIDFIELDER_POSITIONS[i]] = get_player_rating_by_sofifa_id(
+            rating_df, player_id
+        )
+
     # Generate forward stats
     for i, player_id in enumerate(players_in_match["home_team_sofifa_ids"]["forwards"]):
-        home_stats[FORWARD_POSITIONS[i]] = get_player_rating_by_sofifa_id(rating_df, player_id)
+        home_stats[FORWARD_POSITIONS[i]] = get_player_rating_by_sofifa_id(
+            rating_df, player_id
+        )
 
     for i, player_id in enumerate(players_in_match["away_team_sofifa_ids"]["forwards"]):
-        away_stats[FORWARD_POSITIONS[i]] = get_player_rating_by_sofifa_id(rating_df, player_id)
-    
+        away_stats[FORWARD_POSITIONS[i]] = get_player_rating_by_sofifa_id(
+            rating_df, player_id
+        )
+
     home_data_dict = generate_data_dict_for_team(home_stats, away_stats)
     away_data_dict = generate_data_dict_for_team(away_stats, home_stats)
 
     return (home_data_dict, away_data_dict)
-        
 
 
 if __name__ == "__main__":
@@ -340,7 +389,7 @@ if __name__ == "__main__":
     ratings = get_csv_files_in_folder(RATINGS_DIR)
 
     available_seasons = "\n".join(get_seasons_cli())
-    season = input(f"Select a season and hit enter\n{available_seasons}")
+    season = input(f"Select a season and hit enter\n{available_seasons}\n")
 
     if not season.isdigit() or int(season) > len(seasons):
         print("Invalid season selected")
@@ -356,7 +405,9 @@ if __name__ == "__main__":
     rating_df = pd.read_csv(f"{RATINGS_DIR}/{rating_file_name}")
 
     matches = "\n".join(get_matches_cli(matches_df))
-    matchId = input(f"Select a match from {season_file_name} and hit enter\n{matches}")
+    matchId = input(
+        f"Select a match from {season_file_name} and hit enter\n{matches}\n"
+    )
 
     match_name = get_match_name(matches_df, int(matchId) - 1)
 
@@ -368,8 +419,7 @@ if __name__ == "__main__":
 
     home_data_dict, away_data_dict = generate_data_dict(rating_df, players_in_match)
 
-    base_file_name = f'{selected_season}__{match_name}'
+    base_file_name = f"{selected_season}__{match_name}"
 
-    create_pcsp_model(f'{base_file_name}_home.pcsp', home_data_dict)
-    create_pcsp_model(f'{base_file_name}_away.pcsp', away_data_dict)
-
+    create_pcsp_model(f"{base_file_name}_home.pcsp", home_data_dict)
+    create_pcsp_model(f"{base_file_name}_away.pcsp", away_data_dict)
