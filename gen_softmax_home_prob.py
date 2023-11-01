4070d13d4c7647d7e60bdacd6e16b4b03db75783
sheimoria <shem.maleriado@gmail.com>
2023-11-02 03:53:46 +0800

    feat: update softmax function example usage

--- gen_softmax_home_prob.py
+++ gen_softmax_home_prob.py
@@ -8,8 +8,8 @@ def gen_softmax_home_prob(home_prob, away_prob):
 
 # Example usage
 home_prob = gen_prob("20152016_12115_AFC_Bournemouth_Aston_Villa_home")
-print(home_prob)
+print("home_prob: {0}".format(home_prob))
 away_prob = gen_prob("20152016_12115_AFC Bournemouth_Aston Villa_away")
-print(away_prob)
+print("away_prob: {0}".format(away_prob))
 softmax_home_prob = gen_softmax_home_prob(home_prob, away_prob)
-print(softmax_home_prob)
+print("softmax_home_prob: {0}".format(softmax_home_prob))
